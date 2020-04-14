c352c74cd1468126ca599e4d767d482c366c221b
Leonel Elimpe <leonelngande@gmail.com>
2020-04-14 11:50:39 +0100

    Fix template bug related to keyvalue

--- src/app/employees/components/employee-dialog/employee-dialog.component.ts
+++ src/app/employees/components/employee-dialog/employee-dialog.component.ts
@@ -13,7 +13,15 @@ export class EmployeeDialogComponent implements OnInit {
 
   constructor(
     public dialogRef: MatDialogRef<EmployeeDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Employee) {}
+    /**
+     * There's a template compile error (due to usage of keyvalue pipe) when I use Employee interface instead of the below record type.
+     * It's a bug that shouldn't occur so I suspect a bug introduced in Angular 9, have to investigate more and maybe open an issue in
+     * the Angular repository.
+     */
+    @Inject(MAT_DIALOG_DATA) public data: Record<keyof Employee, any>,
+  ) {
+    console.log(data);
+  }
 
   ngOnInit(): void {
   }
